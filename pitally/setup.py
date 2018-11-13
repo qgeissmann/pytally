4a9e6e6619a587635491ccf207270ef86dc71135
quentin geissmann <qgeissmann@gmail.com>
2018-11-13 10:59:50 -0800

    ibid

--- pitally/setup.py
+++ pitally/setup.py
@@ -8,6 +8,5 @@ setup(
     scripts=['bin/pitally.sh'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask',
-                      "Flask-Bootstrap"]
+    install_requires=['Flask']
 )
